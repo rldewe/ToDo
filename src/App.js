f0a7922d548f9a196011c53f28c1457d604b71a6
rldewe <04srishtiy@gmail.com>
2023-07-08 23:39:11 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import TaskForm from './TaskForm';
+import Task from './Task';
+import { useState ,useEffect} from 'react';
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    if (tasks.length === 0) return;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    setTasks(tasks || []);
+  }, []);
+
+  function addTask(name) {
+    setTasks(prev => {
+      return [...prev, {name:name,done:false}];
+    });
+  }
+
+  function removeTask(indexToRemove) {
+    setTasks(prev => {
+      return prev.filter((taskObject,index) => index !== indexToRemove);
+    });
+  }
+
+  function updateTaskDone(taskIndex, newDone) {
+    setTasks(prev => {
+      const newTasks = [...prev];
+      newTasks[taskIndex].done = newDone;
+      return newTasks;
+    });
+  }
+
+  const numberComplete = tasks.filter(t => t.done).length;
+  const numberTotal = tasks.length;
+
+ 
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+            <h2>TO-D0 LIST</h2>
+
+  <h1>{numberComplete}/{numberTotal} Complete</h1>
+    <TaskForm onAdd={addTask}/>
+    {tasks.map((task,index) => (
+        <Task {...task}
+              onTrash={() => removeTask(index)}
+              onToggle={done => updateTaskDone(index, done)} />
+      ))}
+      
+    <Task/>
+
     </div>
   );
 }
